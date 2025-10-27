b52adc05dbf1030a918d72f44f7efa1554bb1a5f
Johanna_VJ <165029467+Chaves-Johanna-Milagro@users.noreply.github.com>
2025-10-26 23:56:48 -0300

    Movimiento vertical
    La paleta se mueve verticalmente  utilizando las teclas w/s o las flechitas

---
+++ PVJ2_Juego_Multijugador/Assets/1ra Version/Scripts/Player/MoveVertical.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class MoveVertical : MonoBehaviour
+{
+    private float _speed = 10f;
+    private float _limitY = 5f;
+    private float _extremesY;
+
+    private Rigidbody2D _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+
+        // Detecta los limites del sprite 
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        _extremesY = sr.bounds.extents.y;
+    }
+
+    void Update()
+    {
+        float input = Input.GetAxisRaw("Vertical"); // W/S o ↑/↓
+        _rb.linearVelocity = new Vector2(0f, input * _speed);
+
+        // Limitar posición vertical
+        Vector2 pos = transform.position;
+        pos.y = Mathf.Clamp(pos.y, -_limitY + _extremesY, _limitY - _extremesY);
+        transform.position = pos;
+    }
+}
